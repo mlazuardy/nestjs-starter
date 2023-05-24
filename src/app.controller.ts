93e131440f07cc3b54e1eef8474c053992b5fe9c
mlazuardy <lazuardymichael@gmail.com>
2023-05-24 20:41:54 +0700

    chore: map error response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, UseFilters } from '@nestjs/common';
 import { TypedBody, TypedRoute } from '@nestia/core';
-import { createValidate } from "typia";
 import { CommonExceptionFilter } from './common.exception-filter';
 
 export interface TestInput {
@@ -19,7 +18,7 @@ export interface TestInput {
 @UseFilters(CommonExceptionFilter)
 export class AppController {
   @TypedRoute.Post()
-  index(@TypedBody({ type: "validate", validate: createValidate<TestInput>() }) data: TestInput) {
+  index(@TypedBody() data: TestInput) {
     return {
       data
     }
--- src/common.exception-filter.ts
+++ src/common.exception-filter.ts
@@ -1,16 +1,22 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { ArgumentsHost, BadRequestException, Catch, ExceptionFilter } from "@nestjs/common";
 import { Response } from "express";
 
-@Catch(HttpException)
+@Catch(BadRequestException)
 export class CommonExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
-    console.log("exception filter called", exception.getResponse());
+  catch(exception: BadRequestException, host: ArgumentsHost) {
     const res = host.switchToHttp().getResponse<Response>();
+    const errResponse = exception.getResponse() as any;
     const status = exception.getStatus();
-    res.status(status).json({
-      statusCode: status,
-      errors: exception.getResponse(),
-      message: "Common exception message"
-    })
+
+    const json: Record<string, any> = {
+      statusCode: status
+    }
+
+    if (typeof errResponse === "object" && errResponse.errors?.length) {
+      json.errors = errResponse.errors;
+      json.message = errResponse.message;
+    }
+
+    return res.status(status).json(json);
   }
 }
